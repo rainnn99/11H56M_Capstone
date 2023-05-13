1c122069e392872883b101908ebcac2fdae6ae38
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-13 14:36:54 +0900

    추가

---
+++ DB_CORD/database_val_return.py
@@ -0,0 +1,34 @@
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#    host="localhost",
+#    user="test",
+#    password="test",
+#    database="testdb"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("SELECT test2 FROM customers WHERE test1 = 2")
+
+# result = mycursor.fetchone()
+
+# print(result[0])
+
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",  # 비밀번호 입력
+    database="food_recommendation"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT test2 FROM customers WHERE test1 = 2")
+
+result = mycursor.fetchone()
+
+print(result[0])
