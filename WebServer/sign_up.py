cc1e72fc83436c0e67febd95ef21eb1eb4f4827c
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-19 01:33:30 +0900

    Update sign_up.py

--- WebServer/sign_up.py
+++ WebServer/sign_up.py
@@ -1,31 +1,29 @@
 from flask import Flask, render_template, url_for, session, request, redirect
 import sys
-import pymysql
+import mysql.connector
 
-
-conn = pymysql.connect(host='localhost',
-                       user='root',
-                       password='',  # 비밀번호
-                       db='food_recommendation',
-                       charset='utf8')
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",  # 비밀번호
+)
+mycursor = mydb.cursor()
+mycursor.execute("USE test")
 
 
 # 회원가입
 def sign_up():
-    id = request.args.get("id")
-    password = request.args.get("password")
-    name = request.args.get("name")
-    birth = request.args.get("birth")
-    sex = request.args.get("sex")
-    phone_number = request.args.get("phone_number")
-    email = request.args.get("eamil")
+    id = request.form['id']
+    password = request.form['password']
+    name = request.form['name']
+    birth = request.form['birth']
+    sex = request.form['sex']
+    phone_number = request.form['phone_number']
+    email = request.form['email']
 
     sql = "INSERT INTO customer (id, password, name, birth, sex, phone_number, email) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = (id, password, name, birth, sex, phone_number, email)
+    mycursor.execute(sql, val)
+    mydb.commit()
 
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute(sql, (id, password, name, birth, sex,
-                        phone_number, email))
-            conn.commit()
-
-    return redirect(url_for("login"))
+    return
