9d9a6d89c8670c4919a156baedf40d27b644221a
ParkJongHyeok99 <jh0310p@naver.com>
2023-05-22 20:32:41 +0900

    Update sign_up.py

--- WebServer/sign_up.py
+++ WebServer/sign_up.py
@@ -4,11 +4,11 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="",  # 비밀번호
+    user="capstone",
+    password="capstone",  # 비밀번호
 )
 mycursor = mydb.cursor()
-mycursor.execute("USE food_recommendation")
+mycursor.execute("USE test")
 
 
 # 회원가입
@@ -16,13 +16,13 @@ def sign_up():
     id = request.form['id']
     password = request.form['password']
     name = request.form['name']
-    birth = request.form['birth']
-    sex = request.form['sex']
+    #birth = request.form['birth']
+    #sex = request.form['sex']
     phone_number = request.form['phone_number']
-    email = request.form['email']
+    #email = request.form['email']
 
-    sql = "INSERT INTO customer (id, password, name, birth, sex, phone_number, email) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    val = (id, password, name, birth, sex, phone_number, email)
+    sql = "INSERT INTO customer (id, password, name, phone_number) VALUES (%s, %s, %s, %s)"
+    val = (id, password, name, phone_number)
     mycursor.execute(sql, val)
     mydb.commit()
 
